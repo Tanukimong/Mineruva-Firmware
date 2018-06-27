2df5a8f0cefbe4bdbb219199e89c209eebe16ec2
Hyeonho Shin <tlsgusghq@hanyang.ac.kr>
2018-06-27 14:45:45 +0900

    원래 쓰던 설정으로 변경

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -936,10 +936,10 @@
   #define MIN_PROBE_EDGE 10
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION 45
-  #define RIGHT_PROBE_BED_POSITION 180
-  #define FRONT_PROBE_BED_POSITION 15
-  #define BACK_PROBE_BED_POSITION (Y_BED_SIZE - 15)
+  #define LEFT_PROBE_BED_POSITION 50
+  #define RIGHT_PROBE_BED_POSITION 160
+  #define FRONT_PROBE_BED_POSITION 10
+  #define BACK_PROBE_BED_POSITION 160
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
